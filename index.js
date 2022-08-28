093600bb96eb5e50742f350a479b4191c73c2d8e
Arif Billah <98398506+ArifBillah6482@users.noreply.github.com>
2022-08-28 10:49:10 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const name_ = document.getElementById("name").value;
+const email = document.querySelector("#email").value;
+const msg = document.querySelector("#msg").value;
+const alertText = document.querySelector(".alertText");
+const form = document.querySelector(".form");
+//////////////////
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alertText.textContent = "Sending...";
+  axios
+    .post("https://contact-arifbillah.herokuapp.com/data", {
+      name: name_,
+      email,
+      msg,
+    })
+    .then(() => {
+      alert("Your information has been sent.");
+      alertText.textContent = "Your information has been sent.";
+    })
+    .catch((err) => console.log(err));
+});
